a825db57229488421f00d590145f1ae5d18112c1
namnguyen080400 <namq.nguyen@bellevuecollege.edu>
2023-04-27 21:54:21 -0700

    first submit

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+var foodItems = [];
+class FoodItem {
+    constructor(foodName, calories) {
+        this.foodName = foodName;
+        this.calories = calories;
+    }
 
-    document.getElementById("button1").addEventListener("click", function () {
+    
+};
 
-        document.getElementById("textbox1").value = "success";
-    });
-});
+function totalCalories() {
+    let foodName = document.getElementById("foodName").value;
+    let calories = document.getElementById("calorie").value;
+    let foodItem = new FoodItem(foodName, calories);
+    foodItems.push(foodItem);
+    let totalCal = 0;
+    for (var i = 0; i < foodItems.length; i++) {
+        let item = foodItems[i];
+        totalCal += parseInt(item.calories);
+    }
+    document.getElementById("totalCalories").value = totalCal;
+    console.log(foodItems);
+
+}
+
+
 
